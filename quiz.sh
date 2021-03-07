22a2405917171604c59e1bb6b7eb6ce3d5555e22
Cole Rau <colevrau@gmail.com>
2021-03-06 18:07:15 -0800

    created first question

--- quiz.sh
+++ quiz.sh
@@ -1,5 +1,17 @@
 quiz() {
-  echo "Started quiz"
+  echo "Starting quiz..."
+  random1=$(($RANDOM / 100))
+  random2=$(($RANDOM / 100))
+  result=$(($random1 + $random2))
+  
+  echo "What is $random1 + $random2?"
+  read response
+  if [ "$response" == $result ]
+  then
+    echo "You got it."
+  else 
+    echo "That is incorrect."
+  fi
 }
 
 echo "Begin math quiz? (y/n)"
